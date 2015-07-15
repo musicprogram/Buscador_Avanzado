7207c74cefa4813852b944cc6cb230234c91c36a
JuanHenao <juan-o-henao@hotmail.es>
2015-07-15 15:13:25 +0000

    Burcador avanzado

---
+++ app/models/book.rb
@@ -0,0 +1,13 @@
+class Book < ActiveRecord::Base
+    
+    def self.search(search)
+       
+        if search   
+            where(["name LIKE ?", "%#{search}%"])
+        else
+            all
+        end
+        
+    end
+    
+end
